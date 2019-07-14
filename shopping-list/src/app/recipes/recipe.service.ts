98d5b6dc3a856cf41f93791865a1058eaf02072c
unknown <dziukers@gmail.com>
2019-07-14 17:48:00 +0200

    added subjects, finished observables

--- shopping-list/src/app/recipes/recipe.service.ts
+++ shopping-list/src/app/recipes/recipe.service.ts
@@ -1,9 +1,10 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Injectable } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A test recipe', 'Just Test', 'https://images.unsplash.com/photo-1541014741259-de529411b96a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80',
@@ -21,11 +22,16 @@ export class RecipeService {
         ])
       ];
 
+      constructor(private shoppingListService: ShoppingListService) {}
       getRecipes() {
           return this.recipes.slice();  // get copy of recipes array
       }
 
       getRecipe(index: number) {
           return this.recipes[index]
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
       }
 }
