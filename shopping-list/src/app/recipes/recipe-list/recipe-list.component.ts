f7ccb90eb553c7516eb867c8861aa9d697ec270f
unknown <dziukers@gmail.com>
2019-09-29 21:22:45 +0200

    finished pipes

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-   this.subscription = this.recipeService.recipesChanged
+    this.subscription = this.recipeService.recipesChanged
     .subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
